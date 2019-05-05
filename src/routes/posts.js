ec2793d92582a8f6cd0ae5e653db1f5539202cb3
Lucas Marques <lucas.msilva09@gmail.com>
2019-05-05 16:38:17 -0300

    Creating a middleware for the files

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -4,37 +4,13 @@ const multer = require("multer");
 const PostController = require("../controllers/posts");
 
 const checkAuth = require("../middleware/check-auth");
+const extractFile = require("../middleware/file");
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg"
-};
+router.post("", checkAuth, extractFile, PostController.createPost);
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    callback(null, "images");
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname
-      .toLowerCase()
-      .split(" ")
-      .join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    callback(null, name + "-" + Date.now() + "." + ext);
-  }
-});
-
-router.post("", checkAuth, multer({ storage: storage }).single("image"), PostController.createPost);
-
-router.put("/:id", checkAuth, multer({ storage: storage }).single("image"), PostController.updatePost);
+router.put("/:id", checkAuth, extractFile, PostController.updatePost);
 
 router.get("", PostController.getPosts);
 
